5f1194cda9bac4334b4daa381c245eee9d56e01f
Liam Pallett <liam.pallett06@gmail.com>
2026-03-15 18:25:14 +0000

    Added README template, MIT license and completed Vector2.hpp implementation with all required methods.

---
+++ README.md
@@ -0,0 +1,65 @@
+# physics-engine
+
+Custom physics engine written in C++ from scratch.
+
+---
+
+# What the Project Is
+
+- What does this project do?
+- Who is it for?
+- What problem does it solve?
+- What makes it interesting or different?
+
+---
+
+# How It Works
+
+- What are the main components of the system?
+- How do the pieces interact?
+- What is the core logic or architecture?
+
+---
+
+# Engineering Decisions
+
+For each non-obvious technical choice made in this project:
+- What decision was made?
+- What were the alternatives considered?
+- Why was this approach chosen over the alternatives?
+- What are the known limitations or tradeoffs of this choice?
+
+# Tech Stack
+
+- Languages used:
+- Frameworks/libraries:
+- Tools and platforms:
+
+---
+
+# What I Learned
+
+- What new skills did I gain?
+- What concepts became clearer through building this?
+- What surprised me during development?
+
+---
+
+# How to Run the Project
+
+- Prerequisites:
+- Installation steps:
+- How to start/run the project:
+- Example commands:
+
+---
+
+# Project Structure
+
+# Future Improvements
+
+- What features would I add next?
+- What parts could be improved or refactored?
+- What ideas did I not have time to implement?
+
+---
--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -2,40 +2,62 @@
 // Created by Liam Pallett on 16/02/2026.
 //
 
-#ifndef NEWTONIAN_ORBIT_MECHANICS_VECTOR2_HPP
-#define NEWTONIAN_ORBIT_MECHANICS_VECTOR2_HPP
+#ifndef PHYSICS_ENGINE_VECTOR2_HPP
+#define PHYSICS_ENGINE_VECTOR2_HPP
 #include <cmath>
 
 struct Vector2 {
     double x;
     double y;
 
-    Vector2 operator+(Vector2 other) const {
+    [[nodiscard]] Vector2 operator+(const Vector2& other) const {
         return Vector2{x + other.x, y + other.y};
     }
 
-    Vector2 operator-(Vector2 other) const {
+    [[nodiscard]] Vector2 operator-(const Vector2& other) const {
         return Vector2{x - other.x, y - other.y};
     }
 
-    Vector2 operator*(double other) const {
+    [[nodiscard]] Vector2 operator*(const double other) const {
         return Vector2{x * other, y * other};
     }
 
-    double length() const {
+    [[nodiscard]] Vector2 operator/(const double other) const {
+        return Vector2{x / other, y / other};
+    }
+
+    Vector2& operator+=(const Vector2& other) {
+        x += other.x;
+        y += other.y;
+
+        return *this;
+    }
+
+    Vector2& operator-=(const Vector2& other) {
+        x -= other.x;
+        y -= other.y;
+
+        return *this;
+    }
+
+    [[nodiscard]] double dot(const Vector2& other) const {
+        return x * other.x + y * other.y;
+    }
+
+    [[nodiscard]] double length() const {
         return std::sqrt(x*x + y*y);
     }
 
-    double lengthSquared() const {
+    [[nodiscard]] double lengthSquared() const {
         return x*x + y*y;
     }
 
-    Vector2 normalise() const {
-        double length = this.length();
-        if (length == 0) return {0,0};
+    [[nodiscard]] Vector2 normalise() const {
+        double len = length();
+        if (len == 0) return {0,0};
 
-        return {this.x / length, this.y / length};
+        return {x / len, y / len};
     }
 };
 
-#endif //NEWTONIAN_ORBIT_MECHANICS_VECTOR2_HPP
+#endif //PHYSICS_ENGINE_VECTOR2_HPP
